88690eb3cbf37b6e933575fa4fae1822558676cf
Muktang <49885170+Muktang@users.noreply.github.com>
2025-05-29 18:32:56 +0530

    Update secrettest.m

--- code/secrettest.m
+++ code/secrettest.m
@@ -1,7 +1,8 @@
 function secrettest
 sprintf("Listing all env variables \n")
 system('env')
-sprintf("Listing all secrets \n")
-[names, metadata] = matlab.authnz.internal.builtins.public.listsecrets
-disp(names);
+disp(getenv("MY_SECRET"));
+%sprintf("Listing all secrets \n")
+%[names, metadata] = matlab.authnz.internal.builtins.public.listsecrets
+%disp(names);
 end
