9323a2f4db94bbbe542da8699df22a63f6b17ae9
Muktang <49885170+Muktang@users.noreply.github.com>
2025-05-31 14:47:43 +0530

    Update secrettest.m

--- code/secrettest.m
+++ code/secrettest.m
@@ -1,7 +1,14 @@
 function secrettest
 sprintf("Listing all env variables \n")
 system('env')
-disp(getenv("MY_SECRET"));
+secretvalue = getenv("MY_SECRET");
+disp(secretvalue);
+
+fprintf('Secret value has length: %d\n', length(secretValue));
+  fprintf('First 5 characters of secret: %s...\n', secretValue(1:min(5, end))); % Example: print first 5 chars
+
+
+% 
 %sprintf("Listing all secrets \n")
 %[names, metadata] = matlab.authnz.internal.builtins.public.listsecrets
 %disp(names);
