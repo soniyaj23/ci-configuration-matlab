b34332cee25d6a16ab2a663521999df42dfaaf11
Muktang <49885170+Muktang@users.noreply.github.com>
2025-06-02 12:33:20 +0530

    Update secrettest.m

--- code/secrettest.m
+++ code/secrettest.m
@@ -1,15 +1,18 @@
 function secrettest
 sprintf("Listing all env variables \n")
 system('env')
-secretValue = getenv("MY_SECRET");
-disp(secretValue);
+secretValue = getenv("MATLAB_APP_KEY");
 
+% GitHub actions actively prevents logging secret values 
 fprintf('Secret value has length: %d\n', length(secretValue));
-  fprintf('First 5 characters of secret: %s...\n', secretValue(1:min(5, end))); % Example: print first 5 chars
+fprintf('First 5 characters of secret: %s...\n', secretValue(1:min(5, end))); % Example: print first 5 chars
 
 
-% 
-%sprintf("Listing all secrets \n")
-%[names, metadata] = matlab.authnz.internal.builtins.public.listsecrets
-%disp(names);
+% Try to access list of Secrets
+sprintf("Listing all secrets \n")
+try
+  [names, metadata] = matlab.authnz.internal.builtins.public.listsecrets
+catch ME
+  disp(ME);
+end
 end
